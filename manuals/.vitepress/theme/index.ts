511c7e4e5536bc60502cc9c60b74a8867017a4ea
kocya <sample@test.co.jp>
2025-11-20 16:34:57 +0900

    update for customization

--- manuals/.vitepress/theme/index.ts
+++ manuals/.vitepress/theme/index.ts
@@ -1,3 +1,4 @@
+/*
 import DefaultTheme from "vitepress/theme";
 import Layout from "./Layout.vue";
 
@@ -5,3 +6,21 @@ export default {
   extends: DefaultTheme,
   Layout,
 };
+*/
+// https://vitepress.dev/guide/custom-theme
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  },
+} satisfies Theme;
